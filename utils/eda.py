f4da986076ea44211e56297de21b0cb7c8702e5e
lmontaldo <montaldo.laura@gmail.com>
2023-06-24 20:25:26 -0300

    eda

--- utils/eda.py
+++ utils/eda.py
@@ -1,9 +1,4 @@
-import sqlite3
-import sys
-import numbers
-import time
-import math
-import pandas as pd
+import data_loader
 import sqlite3
 import sys
 import numbers
@@ -13,13 +8,24 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 from sklearn.preprocessing import StandardScaler
-import data_loader
 #############################################
 # Retrieve the DataFrames from data_loader
 #############################################
 # Specify the database path
 path = 'C:/Users/Transitorio/Desktop/tesis2023/tesis2023-1/data/mydatabase.db'
 # Retrieve the DataFrames from data_loader
-df_clases, df_univariado, df_fmi, df_clases_filter, df_IPC_gral, df_CLASES_IPC, df_EXTERNAL = data_loader.get_data(path)
-#############################################
-print(df_IPC_gral.head())
+#df_clases, df_univariado, df_fmi, df_clases_filter = data_loader.get_data(path)
+tables_list= ['EXTERNAL', 'CLASES_IPC','IPC_gral']
+df_dict = data_loader.get_data(path, tables_list)
+# to df
+df_ext = df_dict['EXTERNAL']
+df_clases=df_dict['CLASES_IPC']
+df_gral = df_dict['IPC_gral']
+############################################
+# IPC GENERAL EDA
+############################################
+print('\n IPC general información de los datos: \n ------------------------------ \n')
+print(df_gral.info())
+# transform ymd column to datetime
+
+
