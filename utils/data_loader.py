09ac606140f9d4cee33b04aa510a30a724130669
lmontaldo <montaldo.laura@gmail.com>
2023-06-24 19:43:40 -0300

    data ts format

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -51,21 +51,27 @@ def query_table_data(conn, table_name):
 def get_data(database_path):
     # Connect to the database
     conn = connect_to_database(database_path)
-    
     # Query 1: Get table data for 'Datosipc'
     table_clases = 'Datosipc'
     df_clases = query_table_data(conn, table_clases)
-    
     # Query 2: Get table data for 'ipc_univariado'
-    table_univa = 'ipc_univariado'
+    table_univa = 'ipc_general_ine'
     df_univariado = query_table_data(conn, table_univa)
-    
     # Query 3: Get table data for 'Datos_fmi_2022_external'
     table_fmi = 'Datos_fmi_2022_external'
     df_fmi = query_table_data(conn, table_fmi)
+    # Query 4: get the ipc clases to keep and filter  
+    table_clases_filter = 'clases_ipc_filtradas'
+    df_clases_filter = query_table_data(conn, table_clases_filter)
+    # Query 5: IpC general log and z-score 
+    table_ipc_gral_re = 'clases_ipc_filtradas'
+    df_clases_filter = query_table_data(conn, table_clases_filter)
+    
     
     # Close the database connection
     conn.close()
     
-    return df_clases, df_univariado, df_fmi
+    return df_clases, df_univariado, df_fmi, df_clases_filter, 
+
+
 
