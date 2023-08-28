4d934bd9087a6db2523bbf99a2110d50cdfb2885
Laura Montaldo <montaldo.lau@gmail.com>
2023-08-28 16:46:44 -0300

    retomando: uso de notebooks

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # File paths
 DATA_DIR = 'data/'
-DATA_BASE_PATH='C:/Users/Transitorio/Desktop/tesis2023/tesis2023-1/data/mydatabase.db'
+DATA_BASE_PATH='C:/Users/user/Desktop/tesis2023-1/data/mydatabase.db'
 LOG_DIR = 'logs/'
 MODEL_DIR = 'models/'
-image_path='C:/Users/Transitorio/Desktop/tesis2023/tesis2023-1/docs/images/'
+image_path='C:/Users/user/Desktop/tesis2023-1/docs/images'
