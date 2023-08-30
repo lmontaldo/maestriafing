835c7ba0b022d75d2f5b6d06047a9ab3b3729f6d
Laura Montaldo <montaldo.lau@gmail.com>
2023-08-30 18:49:41 -0300

    adf y kpss

--- utils/kpss_analyzer.py
+++ utils/kpss_analyzer.py
@@ -2,81 +2,65 @@ import pandas as pd
 from statsmodels.tsa.stattools import kpss
 
 class KPSSAnalyzer:
-    """
-    KPSSAnalyzer performs the Kwiatkowski-Phillips-Schmidt-Shin (KPSS) test
-    for stationarity on time series data in a pandas DataFrame.
-
-    Parameters:
-        dataframe (pandas.DataFrame): The input DataFrame containing the time series data.
-        regressions (list, optional): A list of regression types for the KPSS test.
-                                      Choose 'c' for constant or 'ct' for trend.
-                                      Default is ['c'].
-        nlags (int, str, list, optional): The number of lags to be used for the KPSS test.
-                                         Valid options: 'lshort', 'legacy', 'auto', an integer value, or a list of integers.
-                                         Default is 'auto'.
-
-    Methods:
-        kpss_test(columns=None):
-            Performs the KPSS test on the specified columns.
-
-        run_tests(columns=None):
-            Runs the KPSS test on the specified columns for all combinations of regression types and nlags.
-            If no columns are specified, the test is performed on all columns in the DataFrame.
-    """
-
-    def __init__(self, dataframe, regressions=['c'], nlags='auto'):
-        self.dataframe = dataframe
+    ...
+    def __init__(self, data, regressions=['c'], nlags=None):
+        self.data = data
         self.regressions = regressions
         self.nlags = nlags
 
         self._validate_inputs()
 
     def _validate_inputs(self):
-        valid_regression_options = ['c', 'ct']
-        valid_nlags_options = ['lshort', 'legacy', 'auto']
+        ...
+        if not isinstance(self.data, (pd.DataFrame, pd.Series)):
+            raise ValueError("Invalid data type. Data must be a pandas DataFrame or Series.")
 
-        if not isinstance(self.regressions, list):
-            self.regressions = [self.regressions]
+    def kpss_test(self, regression, nlag, columns=None):
+        if isinstance(self.data, pd.DataFrame):
+            if columns is None:
+                columns = self.data.columns
 
-        for regression in self.regressions:
-            if regression not in valid_regression_options:
-                raise ValueError(f"Invalid regression type '{regression}'. Choose 'c' for constant or 'ct' for trend.")
-
-        if isinstance(self.nlags, int) and self.nlags <= 0:
-            raise ValueError("Invalid value for nlags. It must be a positive integer, a list of integers, or one of 'lshort', 'legacy', or 'auto'.")
-
-        if isinstance(self.nlags, str) and self.nlags not in valid_nlags_options:
-            raise ValueError(f"Invalid value for nlags. Choose one of {valid_nlags_options}, an integer value, or a list of integers.")
-
-        if isinstance(self.nlags, int) or isinstance(self.nlags, str):
-            self.nlags = [self.nlags]
-
-    def kpss_test(self, regression, nlags, columns=None):
-        if columns is None:
-            columns = self.dataframe.columns
-
-        for column in columns:
-            if column not in self.dataframe.columns:
-                raise ValueError(f"Column '{column}' not found in the DataFrame.")
-
-            for nlag in nlags:
+            for column in columns:
+                if column not in self.data.columns:
+                    raise ValueError(f"Column '{column}' not found in the DataFrame.")
+                
                 print(f"Results of KPSS Test for {regression.capitalize()} Stationarity - Column: {column}, nlag: {nlag}")
-                kpsstest = kpss(self.dataframe[column], regression=regression, nlags=nlag, store=True)
+                kpsstest = kpss(self.data[column], regression=regression, nlags=nlag, store=True)
                 kpss_output = pd.Series(
-                    kpsstest[0:3], index=["Test Statistic", "p-value", "Lags Used"]
+                    [kpsstest.stat, kpsstest.p_value, kpsstest.lags],
+                    index=["Test Statistic", "p-value", "Lags Used"]
                 )
                 print("KPSS Test Summary:")
                 print(kpss_output)
 
                 print("Critical Values:")
-                for key, value in kpsstest[3].items():
+                for key, value in kpsstest.critical_values.items():
                     print(f"  {key}: {value}")
 
                 print("------------------")
+        elif isinstance(self.data, pd.Series):
+            print(f"Results of KPSS Test for {regression.capitalize()} Stationarity - nlag: {nlag}")
+            kpsstest = kpss(self.data, regression=regression, nlags=nlag, store=True)
+            kpss_output = pd.Series(
+                [kpsstest.stat, kpsstest.p_value, kpsstest.lags],
+                index=["Test Statistic", "p-value", "Lags Used"]
+            )
+            print("KPSS Test Summary:")
+            print(kpss_output)
+
+            print("Critical Values:")
+            for key, value in kpsstest.critical_values.items():
+                print(f"  {key}: {value}")
+
+            print("------------------")
 
     def run_tests(self, columns=None):
-        if columns is None:
-            columns = self.dataframe.columns
-
-        for regression in self.regressions:
-            self.kpss_test(regression, self.nlags, columns)
+        if isinstance(self.data, pd.DataFrame):
+            if columns is None:
+                columns = self.data.columns
+            
+            for regression in self.regressions:
+                self.kpss_test(regression, self.nlags, columns)
+        elif isinstance(self.data, pd.Series):
+            for regression in self.regressions:
+                self.kpss_test(regression, self.nlags)
