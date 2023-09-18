b54cbdcfdfe0cd802daa7a3d31303c551a2322ce
Laura Montaldo <montaldo.lau@gmail.com>
2023-09-18 14:08:35 -0300

    cambios al 18 de set

--- utils/validators.py
+++ utils/validators.py
@@ -18,17 +18,3 @@ def validate_df(df):
         return False, 'All columns other than the datetime64[ns] column must be float64'
     
     return True, 'DataFrame is valid: contains 1 datetime64[ns] column and all other columns are float64'
-def validate_format(df):
-    # Check the number of columns
-    if df.shape[1] < 2:
-        return 'Cannot determine format from a single column'
-    
-    # Check the data types of the columns
-    dtypes = df.dtypes
-    
-    # If there is exactly one non-numeric column, assume the DataFrame is in wide format
-    if (dtypes != 'object').sum() == df.shape[1] - 1:
-        return 'wide'
-    
-    # If there are multiple non-numeric columns, assume the DataFrame is in long format
-    return 'long'
