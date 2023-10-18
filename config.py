a63a085ca72af079073285afd8dfbdb6b29f7ab2
Laura Montaldo <montaldo.lau@gmail.com>
2023-10-17 21:52:12 -0300

    adf y kpss

--- config.py
+++ config.py
@@ -5,10 +5,11 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Construct paths relative to the root directory
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
-DATA_BASE_PATH = os.path.join(DATA_DIR, '2023-09.csv')
+DATA_BASE_PATH = os.path.join(DATA_DIR, 'datos_fred_procesados.csv')
 LOG_DIR = os.path.join(ROOT_DIR, 'logs')
 MODEL_DIR = os.path.join(ROOT_DIR, 'models')
 IMAGE_PATH = os.path.join(ROOT_DIR, 'docs', 'images')
-PKL_RAW_DATA_PATH = os.path.join(DATA_DIR, 'fred_data.pkl')
-PKL_TRANSF_DATA_PATH = os.path.join(DATA_DIR, 'transf_data_fred_conditions.pkl')
-PKL_TCODES_PATH = os.path.join(DATA_DIR, 'tcodes.pkl')
+#pickled data
+#PKL_RAW_DATA_PATH = os.path.join(DATA_DIR, 'fred_data.pkl')
+#PKL_TRANSF_DATA_PATH = os.path.join(DATA_DIR, 'transf_data_fred_conditions.pkl')
+#PKL_TCODES_PATH = os.path.join(DATA_DIR, 'tcodes.pkl')
---
+++ src/adf_tests.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import sys
+import os
+
+# Append the root directory to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from config import DATA_DIR, DATA_BASE_PATH
+from utils.adf_tests_arch import ModelsADF
+
+df = pd.read_csv(DATA_BASE_PATH, sep=",", index_col='index')
+print(f"Cantidad de columnas a testear: {df.shape[1]}\n")
+adf_model = ModelsADF(df, alpha=0.05)
+results = adf_model.perform_adf_test()
+
+for trend, trend_results in results.items():
+    print("-----------------------------------")
+    print("-----------------------------------")
+    print(f"Results for trend '{trend}':")
+
+    print("ADF Test Results:")
+    print(trend_results.get('df_results'))  # Using .get() to handle missing key gracefully
+
+    # Filter column names for stationary and non-stationary series
+    stationary_series = trend_results.get('Stationary_series', [])
+    non_stationary_series = trend_results.get('Non_stationary_series', [])
+    print("-----------------------------------")
+    print("Stationary Series Count:", len(stationary_series))
+    print("Non-Stationary Series Count:", len(non_stationary_series))
+    print("-----------------------------------")
+
+    # Print the series data for stationary series
+    print("Stationary Series as a List:")
+    print(stationary_series) 
+
+    # Print the non-stationary series as a list
+    print(f"\nNon-Stationary Series as a List:")
+    print(non_stationary_series)
+
+    print()
+
+
+
+
