8b6611742546c5b88082d6c1fa1e41b63e24a136
Laura Montaldo <montaldo.lau@gmail.com>
2023-11-03 18:12:37 -0300

    kpss best

--- src/stage1_preprocessing/step7_split_train_test.py
+++ src/stage1_preprocessing/step7_split_train_test.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import sys
 import os
-import pickle
-from sklearn.decomposition import PCA
+from sklearn.model_selection import TimeSeriesSplit
 import numpy as np
 script_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(script_dir, "..", ".."))
@@ -12,14 +11,23 @@ CSV_Y_PATH = os.path.join(project_root, "data", "prepro", 'sfr_sin_normalizar.cs
 # read data  
 df= pd.read_csv(CSV_Y_PATH, sep=",")
 # split df into train and test
-print(df.columns)
-
-
-
-
+df_idx=df.set_index('index')
+df_idx.index = pd.to_datetime(df_idx.index, format='%Y-%m-%d')
+df_idx.index = df_idx.index.date
+df_idx.sort_index(inplace=True)
+train_size = int(len(df_idx) * 0.8)
+train, test = df_idx.iloc[:train_size], df_idx.iloc[train_size:]
+print(train.index.min(),train.index.max())
+print(test.index.min(),test.index.max())
+print(len(test))
+#
+train.reset_index(inplace=True)
+test.reset_index(inplace=True)
  # save data
-#CSV_Y_PATH = os.path.join(project_root, "data", "prepro", 'sfr_sin_normalizar.csv')
-#df_sfr_sin_idx.to_csv(CSV_Y_PATH, index=False)
+train_PATH = os.path.join(project_root, "data", "prepro", 'sfr_train.csv')
+train.to_csv(train_PATH, index=False)
+test_PATH = os.path.join(project_root, "data", "prepro", 'sfr_test.csv')
+test.to_csv(test_PATH, index=False)
 
 
 
