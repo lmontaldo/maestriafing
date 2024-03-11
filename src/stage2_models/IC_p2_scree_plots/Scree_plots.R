f9f2f198437ff7c8e0b2ced988c685e6048699a9
Laura Montaldo <montaldo.lau@gmail.com>
2024-03-10 21:45:37 -0300

    scree plots

--- src/stage2_models/IC_p2_scree_plots/Scree_plots.R
+++ src/stage2_models/IC_p2_scree_plots/Scree_plots.R
@@ -1,35 +1,29 @@
 loaded_objects <-load("data/Rdata/favar_dfms_output.RData")
 libraries=source("utils/load_libraries.R")
-base_plot_path <- "docs/images/"
-# Open a PNG device
-png(paste0(base_plot_path, "ic_bai_ng.png"), width = 800, height = 600)
-# Create the plot
-plot(ics)
-# Close the device to save the file
-dev.off()
-#
-plot(ics)
-#
-save_screeplot <- function(ics=ics, type, main="para la muestra de entrenamiento", cex.main=2, filename, plot_path = "docs/images/") {
-  # Create the directory if it doesn't exist
-  if (!dir.exists(plot_path)) {
-    dir.create(plot_path, recursive = TRUE)
+
+
+# Set the base plot path
+base_plot_path <- "docs/images/scree_plots/"
+
+# Plot and save each type of scree plot
+plot_types <- c("opt_number", "ev", "pve", "cum.pve")
+
+for (i in seq_along(plot_types)) {
+  png(paste0(base_plot_path, paste0("screeplot_", plot_types[i], ".png")), width = 1000, height = 1200)
+
+  if (plot_types[i] == "opt_number") {
+    plot(ics)
+  } else {
+    screeplot(ics, type = plot_types[i], cex.lab = 1.5)
   }
 
-  # Full path for the plot
-  full_plot_path <- paste0(plot_path, filename)
-
-  # Open a PNG device with the full path
-  png(full_plot_path, width = 800, height = 600)
-
-  # Create the plot
-  screeplot(ics, type = type, main = main, cex.main = cex.main)
-
-  # Close the device to save the file
   dev.off()
 }
 
-# Now call the function for each plot
-save_screeplot(ics, type=  "pve", filename = "screeplot_pve.png")
-save_screeplot(ics, type = "ev",  filename = "screeplot_ev.png")
-save_screeplot(ics, type = "cum.pve",  filename = "screeplot_cum_pve.png")
+########################## sin guardar, si se corren quedan en plots
+
+plot(ics)
+screeplot(ics, type="ev", cex.lab = 1.5)
+screeplot(ics, type="pve", cex.lab = 1.5)
+screeplot(ics, type="cum.pve", cex.lab = 1.5)
+################################
