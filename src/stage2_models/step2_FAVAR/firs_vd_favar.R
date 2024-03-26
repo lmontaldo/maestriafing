8f7f90d710758ffb8329dc4bd1a88f7a401149eb
Laura Montaldo <montaldo.lau@gmail.com>
2024-03-26 16:24:38 -0300

    fir dv desemp

--- src/stage2_models/step2_FAVAR/firs_vd_favar.R
+++ src/stage2_models/step2_FAVAR/firs_vd_favar.R
@@ -1,6 +1,6 @@
 rm(list = ls())
 obj1=load("data/Rdata/favar_dfms_output.RData")
-obj2=load("data/Rdata/favar_estimation_results/results_favar_factor_6.RData")
+obj2=load("data/Rdata/favar_estimation_results/results_favar_factor_7.RData")
 libraries=source("utils/load_libraries.R")
 #
 #
@@ -19,6 +19,7 @@ matriz_s<- as.matrix(data_s)
 matriz_fhat<- as.matrix(F_hat)
 reg_loadings = lm(matriz_s ~ 0 + matriz_fhat + data_s[,"FEDFUNDS"])
 loadings = reg_loadings$coefficients
+save(F_hat,loadings,  file = "data/Rdata/variable_importance/lineal.RData")
 #head(reg_loadings$coefficients)
 #summary(reg_loadings)
 #######################################
@@ -72,7 +73,7 @@ for(i in variables){
   if(transf_cumsum[index] == 5){
     plot(cumsum(IRF[,i]), type ='l',lwd=2, main = variable_names[index],
          ylab= "", xlab="Steps", ylim=range(cumsum(Lower[,i]),cumsum(Upper[,i])),
-         cex.main=1, cex.axis=1)
+         cex.main=1.5, cex.axis=1)
     lines(cumsum(Upper[,i]), lty=2, col="red")
     lines(cumsum(Lower[,i]), lty=2, col="red")
     abline(h=0)
@@ -80,7 +81,7 @@ for(i in variables){
   else{
     plot(IRF[,i], type ='l',lwd=2, main = variable_names[index],
          ylab= "", xlab="Steps", ylim=range((Lower[,i]),(Upper[,i])),
-         cex.main=1, cex.axis=1)
+         cex.main=1.5, cex.axis=1)
     lines((Upper[,i]), lty=2, col="red")
     lines((Lower[,i]), lty=2, col="red")
     abline(h=0)
@@ -100,7 +101,7 @@ irf_X_pc3 = array(c(0,0), dim=c(hor+1, key_nvars))
 irf_X_pc4 = array(c(0,0), dim=c(hor+1, key_nvars))
 irf_X_pc5 = array(c(0,0), dim=c(hor+1, key_nvars))
 irf_X_pc6 = array(c(0,0), dim=c(hor+1, key_nvars))
-#irf_X_pc7 = array(c(0,0), dim=c(hor+1, key_nvars))
+irf_X_pc7 = array(c(0,0), dim=c(hor+1, key_nvars))
 irf_X_ffr = array(c(0,0), dim=c(hor+1, key_nvars))
 for(i in 1:key_nvars){
   irf_X_pc1[,i] = irf_points$irf$X1 %*% matrix(loadings[1:nrow(loadings), variables[i]])
@@ -109,7 +110,7 @@ for(i in 1:key_nvars){
   irf_X_pc4[,i] = irf_points$irf$X4 %*% matrix(loadings[1:nrow(loadings), variables[i]])
   irf_X_pc5[,i] = irf_points$irf$X5 %*% matrix(loadings[1:nrow(loadings), variables[i]])
   irf_X_pc6[,i] = irf_points$irf$X6%*% matrix(loadings[1:nrow(loadings), variables[i]])
-  #irf_X_pc7[,i] = irf_points$irf$f7 %*% matrix(loadings[1:nrow(loadings), variables[i]])
+  irf_X_pc7[,i] = irf_points$irf$X7%*% matrix(loadings[1:nrow(loadings), variables[i]])
   irf_X_ffr[,i] = (irf_points$irf$FEDFUNDS) %*% matrix(loadings[1:nrow(loadings), variables[i]])
 }
 # Get the IRFs squared and accumulate them
@@ -119,7 +120,7 @@ psi2_pc3 = array(0, dim = key_nvars)
 psi2_pc4 = array(0, dim = key_nvars)
 psi2_pc5 = array(0, dim = key_nvars)
 psi2_pc6 = array(0, dim = key_nvars)
-#psi2_pc7 = array(0, dim = key_nvars)
+psi2_pc7 = array(0, dim = key_nvars)
 psi2_ffr = array(0, dim = key_nvars)
 
 for(i in 1:key_nvars){
@@ -130,7 +131,7 @@ for(i in 1:key_nvars){
     psi2_pc4[i] = psi2_pc4[i] + irf_X_pc4[j,i]^2
     psi2_pc5[i] = psi2_pc5[i] + irf_X_pc5[j,i]^2
     psi2_pc6[i] = psi2_pc6[i] + irf_X_pc6[j,i]^2
-    #psi2_pc7[i] = psi2_pc7[i] + irf_X_pc7[j,i]^2
+    psi2_pc7[i] = psi2_pc7[i] + irf_X_pc7[j,i]^2
     psi2_ffr[i] = psi2_ffr[i] + irf_X_ffr[j,i]^2
   }
 }
@@ -153,7 +154,7 @@ table = data.frame("PC1" = round((psi2_pc1),3),
                    "PC4" = round((psi2_pc4),3),
                    "PC5" = round((psi2_pc5),3),
                    "PC6" = round((psi2_pc6),3),
-                   #"PC7" = round((psi2_pc7),3),
+                   "PC7" = round((psi2_pc7),3),
                    "FEDFUNDS" = round((psi2_ffr),3),
                    "Factor_Y_total" = round(var_fac,3) ,
                    "e" = round((var_e),3),
@@ -167,107 +168,4 @@ for(i in 1:key_nvars){
 table_favar = data.frame("Variables" = variable_names, "Contribution" = round((psi2_ffr/var_total),3), "R-squared" = round(r2,3))
 table_favar$DV=table_favar["Contribution"]*table_favar["R.squared"]
 xtable(table_favar, digits=3)
-
-#############################
-###### VI
-################################
-load("data/Rdata/ng_dataframe/ng.RData")
-class(loadings)
-abs_loadings <- abs(loadings)
-# Initialize a list to store dataframes
-df_list <- list()
-# Loop through each row name in the loadings matrix
-for (row_name in rownames(loadings)) {
-  # Create a dataframe for the current row
-  df <- data.frame(variable = colnames(loadings),
-                   abs_value = abs(loadings[row_name, ]))
-
-  # Order the dataframe by abs_value in descending order
-  df <- df[order(-df$abs_value), ]
-
-  # Assign the row name as the name of the dataframe
-  names(df) <- c("variable", "abs_value")
-  row.names(df) <- NULL
-  df$abs_value=round(df$abs_value,3)
-  df_list[[row_name]] <- df
-}
-
-# Access a specific dataframe (for example, matriz_fhat1)
-f1_vi <- head(df_list[["matriz_fhat1"]],15)
-f2_vi <- head(df_list[["matriz_fhat2"]],15)
-f3_vi <- head(df_list[["matriz_fhat3"]],15)
-f4_vi <- head(df_list[["matriz_fhat4"]],15)
-f5_vi <- head(df_list[["matriz_fhat5"]],15)
-f6_vi <- head(df_list[["matriz_fhat6"]],15)
-#f7_vi <- head(df_list[["matriz_fhat7"]],15)
-# View the dataframe
-f1_vi
-f2_vi
-f3_vi
-f4_vi
-f5_vi
-f6_vi
-#f7_vi
-
-#########
-ng$fred <- gsub("S.P.div.yield", "S&P div yield", ng$fred)
-# Create gsi column in f2_vi using matched values from ng$gsi
-f1_vi$gsi <- ng$gsi[match(f1_vi$variable, ng$fred)]
-f2_vi$gsi <- ng$gsi[match(f2_vi$variable, ng$fred)]
-f3_vi$gsi <- ng$gsi[match(f3_vi$variable, ng$fred)]
-f4_vi$gsi <- ng$gsi[match(f4_vi$variable, ng$fred)]
-f5_vi$gsi <- ng$gsi[match(f5_vi$variable, ng$fred)]
-f6_vi$gsi <- ng$gsi[match(f6_vi$variable, ng$fred)]
-#f7_vi$gsi <- ng$gsi[match(f7_vi$variable, ng$fred)]
-##########
-# Function to add ng$group to each dataframe
-add_ng_group <- function(df, ng) {
-  # Match variable names between dataframe and ng$fred
-  match_indices <- match(df$variable, ng$fred)
-
-  # Create new columns for ng$group and ng$gsi
-  df$group <- ng$group[match_indices]
-  df$gsi <- ng$gsi[match_indices]
-
-  # Return the updated dataframe
-  return(df)
-}
-
-# Add ng$group and ng$gsi to each dataframe
-f1_vi_group <- add_ng_group(f1_vi, ng)
-f2_vi_group <- add_ng_group(f2_vi, ng)
-f3_vi_group <- add_ng_group(f3_vi, ng)
-f4_vi_group <- add_ng_group(f4_vi, ng)
-f5_vi_group <- add_ng_group(f5_vi, ng)
-f6_vi_group <- add_ng_group(f6_vi, ng)
-
-###############
-
-f1_vi_group$abs_value <- as.numeric(gsub(",", ".", f1_vi_group$abs_value))
-# Verify the conversion
-print(f1_vi_group$abs_value)
-
-
-
-
-
-################################################################
-# Create a horizontal bar plot
-barplot(f1_vi_group$abs_value, horiz = TRUE, names.arg = f1_vi_group$fred,
-        main = "Top 15 variables que contribuyen al primer factor", xlab = "Suma de los valores absolutos por columna",
-        cex.names = 0.7, las = 1, col =  viridis_pal()(15))
-#################################### PLOT F1: 15 VARIABLES MAS IMPORTANTES
-# Set the width of the plotting area
-options(OutDec = ",") # Change the decimal point character
-par(pin = c(8, 4))  # Adjust the width as needed
-
-# Set the margin to accommodate longer names
-par(mar = c(5, 4, 4, 2) + 0.1)
-
-# Create a horizontal bar plot with the matching values as names
-barplot(f1_vi_group$abs_value, horiz = TRUE, names.arg = f1_vi_group$gsi,
-        main = "Top 15 variables que contribuyen al primer factor (F1)",
-        xlab = "Suma de los valores absolutos de los valores kernelshap por columna",
-        cex.names = 0.7, las = 1)
-
 
